7d0b3d759936a5905384dc1edebb80beccb8b8cd
alexisc <a.chentreu@protonmail.com>
2023-10-04 15:56:35 +0200

    Todo followers

--- resoc_n1/followers.php
+++ resoc_n1/followers.php
@@ -37,11 +37,15 @@
                 // Etape 4: à vous de jouer
                 //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous 
                 ?>
+                <?php
+                while ($post = $lesInformations->fetch_assoc()) {
+                ?>
                 <article>
                     <img src="user.jpg" alt="blason"/>
-                    <h3>Béatrice</h3>
-                    <p>id:321</p>
+                    <h3>Prénom : <?php echo($post['alias']) ?></h3>
+                    <p>id : <?php echo($post['id']) ?></p>                    
                 </article>
+                <?php } ?>
             </main>
         </div>
     </body>
