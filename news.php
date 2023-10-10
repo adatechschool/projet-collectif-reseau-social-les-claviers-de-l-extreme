e0daed01a98adf5a8d391e79931d65a3545b351d
MahinGugu <mahinthan.gugananthan@gmail.com>
2023-10-10 11:22:50 +0200

    changement page subscriptions

--- news.php
+++ news.php
@@ -21,6 +21,7 @@
                     <li><a href="settings.php?user_id=5">Paramètres</a></li>
                     <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
                     <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="usurpedpost.php?user_id=5">Opération Croissantage</a></li>
                 </ul>
             </nav>
         </header>
