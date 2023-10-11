a216f88fd66957e003a48ae0c396774d3da683f6
MahinGugu <mahinthan.gugananthan@gmail.com>
2023-10-12 00:43:51 +0200

    add pictures et maj phpmyadmin

--- photoDeProfil.php
+++ photoDeProfil.php
@@ -6,10 +6,8 @@ $reponse = $lesInformations->fetch_assoc();
 
 if ($reponse['photo'] != "0") {
     echo "<img src='" . $user['photo'] . "'/>";
-    print ("c'est bon !");
 } else {
     echo "<img src='Images\Aleatoire.jpg'/>";
-    print ("c'est pas bon !");
 }
 echo "</div>"
 ?>
